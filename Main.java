4ac9132d7dd72720db40770ed0c654c28f46e70e
gabrielestudos1503 <gabrielribeiro1503@edu.unifil.br>
2025-08-11 20:52:16 -0300

    Validacoes do polinomio

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,14 +18,59 @@ public class Main {
         JLabel polinomioLabel = new JLabel("f(x): ");
         polinomioLabel.setBounds(50, 40, 100, 20);
         polinomioLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        polinomioLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         panel.add(polinomioLabel);
 
         JTextField polinomioTextField = new JTextField();
-        polinomioTextField.setBounds(175, 40, 200, 30);
+        polinomioTextField.setBounds(120, 40, 200, 30);
         panel.add(polinomioTextField);
+
+        JButton teste = new JButton("Mostrar");
+        teste.setBounds(350, 40, 100, 30);
+        panel.add(teste);
+
+        teste.addActionListener(e -> {
+            calcularPolinomio(polinomioTextField.getText(), 1);
+        });
 
         frame.add(panel);
         frame.setVisible(true);
+    }
+
+    public static void buscarIntervalos(String polinomio) {
+        for (int i = -1000; i < 1000; i++) {
+
+        }
+    }
+
+    public static void calcularPolinomio(String polinomio, int variavel) {
+        // Exemplo de polinomio: x^3-9*x+3
+
+        // Validação e nom
+        polinomio = polinomio.replace(" ", "");
+        polinomio = polinomio.toLowerCase();
+        polinomio = polinomio.replace("*", "");
+
+        if (polinomio.contains("+-") || polinomio.contains("-+")) {
+            polinomio = polinomio.replace("+-", "-");
+            polinomio = polinomio.replace("-+", "-");
+        }
+        if (polinomio.contains("++") || polinomio.contains("--")) {
+            polinomio = polinomio.replace("++", "+");
+            polinomio = polinomio.replace("--", "+");
+        }
+
+        polinomio = polinomio.replaceAll("([+\\-])x(?=(?:\\^|[+\\-]|$))", "$11x");
+        polinomio = polinomio.replaceAll("^x(?=(?:\\^|[+\\-]|$))", "+1x");
+
+        String[] formas = polinomio.split("(?=[+\\-])");
+
+
+
+        for(String f : formas){
+            System.out.println(f);
+        }
+        System.out.println(polinomio);
+
+        // 1^3-9*1+3
     }
 }
