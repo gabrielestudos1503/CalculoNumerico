b644b5c3954fa2cee2ecb8d9572bbeda4268a5af
gabrielestudos1503 <gabrielribeiro1503@edu.unifil.br>
2025-09-08 01:31:47 -0300

    Metodo de Bissecao finalizado com front

--- Bissecao/Main.java
+++ Bissecao/Main.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     static List<double[]> testeIntervalos = new ArrayList<>();
+    static Map<String, Number> resultados;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Método de Bisseção");
@@ -16,7 +22,7 @@ public class Main {
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        // Inserir Polinomio
+        // Inserir Informações
 
         JLabel polinomioLabel = new JLabel("f(x): ");
         polinomioLabel.setBounds(50, 40, 100, 20);
@@ -27,20 +33,130 @@ public class Main {
         polinomioTextField.setBounds(120, 40, 200, 30);
         panel.add(polinomioTextField);
 
-        JButton teste = new JButton("Mostrar");
-        teste.setBounds(350, 40, 100, 30);
+        JLabel precisaoLabel = new JLabel("Precisão: ");
+        precisaoLabel.setBounds(350, 40, 100, 20);
+        precisaoLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(precisaoLabel);
+
+        JTextField precisaoTextField = new JTextField();
+        precisaoTextField.setBounds(450, 40, 100, 30);
+        panel.add(precisaoTextField);
+
+        // Botão de calcular
+
+        JButton teste = new JButton("Intervalos");
+        teste.setBounds(170, 380, 100, 30);
         panel.add(teste);
 
+        // Lista dos intervalos
+
+        String[] colunas = { "Intervalos" };
+        DefaultTableModel model = new DefaultTableModel(colunas, 0) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        JTable tabela = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(tabela);
+        scrollPane.setBounds(120, 100, 200, 250);
+        frame.add(scrollPane);
+
         teste.addActionListener(e -> {
-            // testando intervalos
             testeIntervalos = buscarIntervalos(polinomioTextField.getText());
             for (double[] par : testeIntervalos) {
                 System.out.printf("[%.0f, %.0f]%n", par[0], par[1]);
+
+                model.addRow(new Object[] { String.format("[%.0f, %.0f]%n", par[0], par[1])
+                });
+            }
+        });
+
+        // Seleção dos intervalos
+
+        JLabel resultadoLabel = new JLabel("Resultado: ");
+        resultadoLabel.setBounds(350, 100, 300, 20);
+        resultadoLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(resultadoLabel);
+
+        JLabel erroLabel = new JLabel("Erro: ");
+        erroLabel.setBounds(350, 150, 300, 20);
+        erroLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(erroLabel);
+
+        JLabel iteracaoLabel = new JLabel("Iteração: ");
+        iteracaoLabel.setBounds(350, 200, 100, 20);
+        iteracaoLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(iteracaoLabel);
+
+        JButton usarBotao = new JButton("Calcular");
+        usarBotao.setBounds(600, 40, 100, 30);
+        panel.add(usarBotao);
+
+        usarBotao.addActionListener(e -> {
+            int viewRow = tabela.getSelectedRow();
+            if (viewRow == -1) {
+                JOptionPane.showMessageDialog(frame, "Selecione um intervalo.");
+                return;
+            }
+            int row = tabela.convertRowIndexToModel(viewRow);
+
+            String s = (String) model.getValueAt(row, 0);
+            Matcher m = Pattern.compile("\\[\\s*([-\\d.]+)\\s*,\\s*([-\\d.]+)\\s*\\]").matcher(s);
+            if (m.find()) {
+                double esquerda = Double.parseDouble(m.group(1));
+                double direita = Double.parseDouble(m.group(2));
+                System.out.printf("Selecionado: [%.2f, %.2f]%n", esquerda, direita);
+
+                if (precisaoTextField.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(frame, "Insira uma precisão.");
+                    return;
+                }
+
+                resultados = bissecao(esquerda, direita, Double.parseDouble(precisaoTextField.getText()),
+                        polinomioTextField.getText());
+                resultadoLabel.setText("Resultado: " + resultados.get("xn"));
+                erroLabel.setText("Erro: " + resultados.get("erro"));
+                iteracaoLabel.setText("Iteração: " + resultados.get("iteracao"));
+
             }
         });
 
         frame.add(panel);
         frame.setVisible(true);
+    }
+
+    public static Map<String, Number> bissecao(double Xa, double Xb, double precisao, String polinomio) {
+        // Mesmo a atividade pedindo para printar na tela apenas os resultados finais
+        // dos intervalos, decidi fazer um sout para teste no prompt
+        double fXa = calcularPolinomio(polinomio, Xa);
+        double fXb = calcularPolinomio(polinomio, Xb);
+
+        int iteracao = 1;
+        double media = (Xa + Xb) / 2.0;
+        double fxn = calcularPolinomio(polinomio, media);
+        double erro = Xb - Xa;
+
+        System.out.println(iteracao + " | " + Xa + " | " + Xb + " | " + media + " | " + fxn + " | " + erro);
+
+        while (erro > precisao) {
+            if (fXa * fxn <= 0) {
+                Xb = media;
+                fXb = fxn;
+            } else {
+                Xa = media;
+                fXa = fxn;
+            }
+            iteracao++;
+            media = (Xa + Xb) / 2.0;
+            fxn = calcularPolinomio(polinomio, media);
+            erro = Xb - Xa;
+            System.out.println(iteracao + " | " + Xa + " | " + Xb + " | " + media + " | " + fxn + " | " + erro);
+
+        }
+
+        return Map.of("xn", media, "iteracao", iteracao, "erro", erro);
+
     }
 
     public static List<double[]> buscarIntervalos(String polinomio) {
